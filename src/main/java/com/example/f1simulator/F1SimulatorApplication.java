9d44e1a0d08c4c72d0dea75dc79721c2146c73fd
Heikki Jarvinen <h.e.jarvinen@gmail.com>
2025-06-24 15:37:08 +0300

    initial commit: setup f1 simulation project

---
+++ src/main/java/com/example/f1simulator/F1SimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.f1simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1SimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1SimulatorApplication.class, args);
+	}
+
+}
